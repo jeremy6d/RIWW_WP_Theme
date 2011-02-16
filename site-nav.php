90da4de62aae24db9ca246f11ff196106283d5f8
Jeremy Weiland <jeremy6d@gmail.com>
2011-02-16 10:36:10 -0500

    initial

---
+++ site-nav.php
@@ -0,0 +1,11 @@
+
+<link href="iww.css" rel="stylesheet" type="text/css" />
+	<ul>
+		<li class="buttons"><a href="/blog">news</a></li>
+	    <li class="buttons"><a href="/calendar.php">calendar</a></li>
+        <li class="buttons"><a href="/community.php">community</a></li>
+        <li class="buttons"><a href="/about.php">about</a></li>
+          <li class="buttons"><a href="/join.php">join</a></li>
+      <li class="buttons"><a href="/book.php">book</a></li>
+        <li class="buttons"><a href="/resources.php">resources</a></li>
+	</ul>
---
+++ socialmediaicons.php
@@ -0,0 +1,7 @@
+
+<link href="iww.css" rel="stylesheet" type="text/css" />
+	<ul>
+		<li class="icon"><a href="/blog"><img src="images/facbook.png" /></a></li>
+	    <li class="icon"><a href="/calendar.php"><img src="images/twittericon.png" /></a></li>
+        <li class="icon"><a href="/community.php"><img src="images/rssicon.png" /></a></li>
+	</ul>
